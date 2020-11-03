ebfebd5ae4384b12c78538c774a53644845944cd
wlozan00 <73435610+wlozan00@users.noreply.github.com>
2020-11-03 19:40:14 +0100

    Update drugi.c

--- drugi.c
+++ drugi.c
@@ -1,74 +1,125 @@
-#include <stdio.h>
+include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct _osoba *Pozicija;
-typedef struct _osoba
+typedef struct _list *position;
+typedef struct _list
 {
-    char ime[20];
-    char prezime[20];
-    int godina;
-    Pozicija next;
+    char name[20];
+    char sname[20];
+    int year;
+    position next;
+} list;
 
-} Osoba;
-
-int ispisi(Osoba *);
-int unosP(Osoba *);
-int unosK(Osoba *);
-Pozicija find(char *, Osoba *);
+int unosP(position);
+int ispis(position);
+int unosK(position);
+position find(char *, position);
+int brisi(char *, position);
 
 int main()
 {
-    Osoba *head;
-    head = (Osoba *)malloc(sizeof(Osoba));
-    head->next = NULL;
-    printf("Unesi nove studente:\n");
-    unosP(head);
-    unosK(head);
-    ispisi(head->next);
-    char a[20];
-    scanf("%s", a);
-    Pozicija trazeni = find(a, head);
-    printf("%s %s %d\n", trazeni->ime, trazeni->prezime, trazeni->godina);
+    position head = NULL;
+    head = (position)malloc(sizeof(list));
+
+    char s1[20];
+    char s2[20];
+    position element = NULL;
+
+    printf("Unesi nove strukture na pocetak liste:\n");
+    for (int i = 0; i < 2; i++)
+    {
+        unosP(head);
+    }
+
+    printf("Unesi nove strukture na kraj liste:\n");
+    for (int i = 0; i < 2; i++)
+    {
+        unosK(head);
+    }
+    puts("\n");
+
+    ispis(head->next);
+
+    puts("\n");
+    printf("Unesi prezime studenta kojeg trazis:\n");
+    scanf("%s", s1);
+    element = find(s1, head->next);
+    printf("Trazeni student:\n");
+    printf("%s %s %d\n", element->name, element->sname, element->year);
+
+    printf("Unesi ime studenta kojeg zelis izbrisati:\n");
+    scanf("%s", s2);
+    brisi(s2, head->next);
+
+    ispis(head->next);
+
+    free(head);
     return 0;
 }
 
-int ispisi(Osoba *temp)
+int unosP(position temp)
+{
+    position new = NULL;
+    new = (position)malloc(sizeof(list));
+    if (new == NULL)
+    {
+        perror("Greska pri alokaciji memorije\n");
+        return -1;
+    }
+    scanf("%s %s %d", new->name, new->sname, &new->year);
+    new->next = temp->next;
+    temp->next = new;
+    return 0;
+}
+
+int ispis(position temp)
 {
     while (temp != NULL)
     {
-        printf("%-10s %-10s %-d\n", temp->ime, temp->prezime, temp->godina);
+        printf("%s\t%s\t%d\n", temp->name, temp->sname, temp->year);
         temp = temp->next;
     }
     return 0;
 }
-int unosP(Osoba *head)
-{
-    Osoba *new;
-    new = (Osoba *)malloc(sizeof(Osoba));
-    scanf("%s %s %d", new->ime, new->prezime, &new->godina);
 
-    new->next = head->next;
-    head->next = new;
-
-    return 0;
-}
-int unosK(Osoba *head)
+int unosK(position temp)
 {
-    while (head->next != NULL)
+    while (temp->next != NULL)
     {
-        head = head->next;
+        temp = temp->next;
     }
 
-    unosP(head);
-
+    unosP(temp);
     return 0;
 }
-Pozicija find(char *x, Pozicija head)
+
+position find(char *tmp, position help)
 {
-    while (strcmp(head->next->prezime, x) != 0 && head->next != NULL)
+    while (help != NULL && strcmp(help->sname, tmp) != 0) //koristi ovu funkciju za provjeru stringova, vraca 0 ako su identicni
     {
-        head = head->next;
+        help = help->next;
     }
-    return head->next;
+    if (help == NULL)
+    {
+        printf("Element nije u listi!\n");
+        return NULL;
+    }
+    return help;
+}
+
+int brisi(char *tmp, position help)
+{
+    position prev = help;
+    while (help != NULL && strcmp(help->sname, tmp) != 0)
+    {
+        prev = help;
+        help = help->next;
+    }
+    if (help != NULL)
+    {
+        prev->next = help->next;
+        free(help);
+    }
+    return 0;
 }
